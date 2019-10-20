d38898ff981334a46ea87e6913bdd7be2c01784d
VidarVigg <vidar.martensson@outlook.com>
2019-10-20 13:17:56 +0200

    Lost Scripts

---
+++ Assets/IsHitState.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsHitState : State
+{
+    private EnemyMaster enemy;
+    private Rigidbody2D enemyRigidbody;
+    private float tick;
+    private float stunTime = 0.5f;// move to config file
+
+    public IsHitState(Character character) : base(character)
+    {
+        this.enemy = (EnemyMaster)character;
+    }
+
+    public override void EnterState()
+    {
+        enemyRigidbody = enemy.Rigidbody;  
+        enemyRigidbody.constraints = RigidbodyConstraints2D.None;
+        enemyRigidbody.AddForce(((enemy.transform.position - enemy.GetTarget().position).normalized) * 1000);
+        enemyRigidbody.gravityScale = 1;
+        Debug.Log("Entered Ragdoll State");
+        Debug.Log("kjskefsjefksfkksef");
+    }
+
+    public override void Update()
+    {
+        if ((tick += Time.deltaTime) >= stunTime)
+        {
+            Debug.Log("sefsesff");
+            tick -= stunTime;
+            enemy.isHit = false;
+            enemy.transform.rotation = Quaternion.identity;
+            enemy.UpdateCurrentState(new PatrolState(character));
+
+        }
+        enemy.transform.Rotate(0, 0, Random.Range(1, 10));
+        for (int i = 0; i < enemy.GetWallCollisionArray().Length; i++)
+        {
+            if (enemy.GetWallCollisionArray()[3].collider != null)
+            {
+                if (enemy.isHit)
+                {
+                    enemy.transform.rotation = Quaternion.identity;
+                    enemy.UpdateCurrentState(this);
+                }
+            }
+        }
+
+    }
+
+    public override void ExitState()
+    {
+        enemy.transform.rotation = Quaternion.identity;
+        enemy.Rigidbody.constraints = RigidbodyConstraints2D.FreezeRotation;
+        enemy.Rigidbody.gravityScale = 0;
+    }
+
+}
---
+++ Assets/PlayerIsHitState.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerIsHitState : State
+{
+    private PlayerMaster player;
+
+    public PlayerIsHitState(Character character) : base(character)
+    {
+        this.player = (PlayerMaster)character;
+    }
+
+    public override void EnterState()
+    {
+        Debug.Log("PlayerIsHit");
+        player.Rigidbody2D.constraints = RigidbodyConstraints2D.None;
+        player.Rigidbody2D.gravityScale = 1;
+        InputManager.INSTANCE.moveDelegate -= player.movementController.Move;  
+        player.movementController.BaseMovementSpeed = 0;
+        
+    }
+
+    public override void Update()
+    {
+
+    }
+
+    public override void ExitState()
+    {
+
+    }
+
+}
